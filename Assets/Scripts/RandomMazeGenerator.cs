6d57748c6ff3f4810547f6c826627aec559a551f
soheilsln <soheil.salehinia@gmail.com>
2021-10-25 16:29:38 +0100

    RandomMazeGenerator Script Finished

--- Assets/Scripts/RandomMazeGenerator.cs
+++ Assets/Scripts/RandomMazeGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RandomMazeGenerator : MonoBehaviour
@@ -60,27 +61,83 @@ public class RandomMazeGenerator : MonoBehaviour
         //Start from a random cell
         ILocation = Random.Range(0, width);
         JLocation = Random.Range(0, height);
-
+        VisitNewCell(cells, ILocation, JLocation);
         //
     }
 
     public void VisitNewCell(Cell[,] cells, int i, int j)
     {
+        Debug.Log((i + 1) + " " + (j + 1));
         cells[i, j].isVisited = true;
-        if(cells[i,j].hasLeftWall == false && cells[i, j].hasLeftWall == false && cells[i, j].hasLeftWall == false && cells[i, j].hasLeftWall == false)
+        if (!HaveAdjacentAvailableCell(cells, i, j))
         {
             return;
         }
-        //To Do
+        while(HaveAdjacentAvailableCell(cells, i, j))
+        {
+            bool wallFound = false;
+            while (!wallFound)
+            {
+                int selectedWall = Random.Range(0, 4);
+                if (cells[i, j].hasWalls[selectedWall])
+                {
+                    if (selectedWall == 0 && i > 0 && !cells[i - 1, j].isVisited)
+                    {
+                        wallFound = true;
+                        cells[i, j].hasWalls[selectedWall] = false;
+                        VisitNewCell(cells, i - 1, j);
+                    }
+                    else if (selectedWall == 1 && j > 0 && !cells[i, j - 1].isVisited)
+                    {
+                        wallFound = true;
+                        cells[i, j].hasWalls[selectedWall] = false;
+                        VisitNewCell(cells, i, j - 1);
+                    }
+                    else if (selectedWall == 2 && i + 1 < width && !cells[i + 1, j].isVisited)
+                    {
+                        wallFound = true;
+                        cells[i, j].hasWalls[selectedWall] = false;
+                        VisitNewCell(cells, i + 1, j);
+                    }
+                    else if (selectedWall == 3 && j + 1 < height && !cells[i, j + 1].isVisited)
+                    {
+                        wallFound = true;
+                        cells[i, j].hasWalls[selectedWall] = false;
+                        VisitNewCell(cells, i, j + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    public bool HaveAdjacentAvailableCell(Cell[,] cells, int i, int j)
+    {
+        if (cells[i, j].hasWalls.All(p => !p))
+            return false;
+        if (i > 0 && !cells[i - 1, j].isVisited)
+            return true;
+        else if (j > 0 && !cells[i, j - 1].isVisited)
+            return true;
+        else if (i + 1 < width && !cells[i + 1, j].isVisited)
+            return true;
+        else if (j + 1 < height && !cells[i, j + 1].isVisited)
+            return true;
+        return false;
     }
 
     public class Cell
     {
-        public bool hasLeftWall = true;
-        public bool hasDownWall = true;
-        public bool hasRightWall = true;
-        public bool hasUpWall = true;
-        public bool isVisited = false;
+        public bool[] hasWalls = new bool[4]; //Left,Down,Right,Up
+        public bool isVisited;
+
+        public Cell()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                hasWalls[i] = true;
+            }
+            isVisited = false;
+        }
     }
 
 }
