82ef07d18b8c3c9e2b4fd8719e1568eb0dca4934
Soheil Salehinia <soheil.salehinia@gmail.com>
2021-11-13 16:42:12 +0000

    AI continued

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -19,17 +19,53 @@ public class AIManager : MonoBehaviour
         visitedCells = new List<int[]>();
         path.Add(initialLocation);
         visitedCells.Add(initialLocation);
+        ChoosePath(initialLocation);
         //Debug.Log(initialLocation[0] + " " + initialLocation[1]);
     }
 
     void Update()
     {
-        
+
     }
 
-    public void choosePath(int iLocation, int jLocation)
+    public void ChoosePath(int[] location)
     {
-        
+        List<int> availablePaths = new List<int>();
+        List<int[]> availableCells = new List<int[]>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (!cells[location[0], location[1]].hasWalls[i])
+            {
+                availablePaths.Add(i);
+            }
+        }
+
+        foreach (int path in availablePaths)
+        {
+            if (path == 0)
+            {
+                availableCells.Add(new int[] { location[0] - 1, location[1] });
+            }
+            else if (path == 1)
+            {
+                availableCells.Add(new int[] { location[0], location[1] - 1 });
+            }
+            else if (path == 2)
+            {
+                availableCells.Add(new int[] { location[0] + 1, location[1] });
+            }
+            else
+            {
+                availableCells.Add(new int[] { location[0], location[1] + 1 });
+            }
+        }
+
+        foreach (int[] cell in availableCells)
+        {
+
+        }
+
     }
 
     public void chooseAction()
