e7f5b345d429d4b0807b918049e2755c0cbaec8f
soheilsln <soheil.salehinia@gmail.com>
2021-11-15 17:59:21 +0000

    Continued on AI

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -10,21 +10,44 @@ public class AIManager : MonoBehaviour
     private int[] currentLocation;
     private List<int[]> path;
     private List<int[]> visitedCells;
+    private List<int[]> exitPath;
+    private bool exitFound = false;
+    private bool keyFound = false;
     private int[] exitLocation;
+    private int[] keyLocation1;
+    private int[] keyLocation2;
 
     void Start()
     {
         cells = RandomMazeGenerator.instance.cells;
         currentLocation = GameManager.instance.initialLocation;
         exitLocation = GameManager.instance.exitLocation;
+        keyLocation1 = GameManager.instance.keyLocation1;
+        keyLocation2 = GameManager.instance.keyLocation2;
         path = new List<int[]>();
         visitedCells = new List<int[]>();
+        exitPath = new List<int[]>();
         path.Add(currentLocation);
         visitedCells.Add(currentLocation);
-        while (!currentLocation.SequenceEqual(exitLocation))
+
+
+        while (!currentLocation.SequenceEqual(keyLocation1) && !currentLocation.SequenceEqual(keyLocation2))
+        {
+            if(currentLocation.SequenceEqual(exitLocation))
+            {
+                //exitPath.Add(currentLocation);
+                exitFound = true;
+            }
+            ChoosePath(currentLocation);
+        }
+        keyFound = true;
+        while (!currentLocation.SequenceEqual(exitLocation) && !exitFound)
         {
             ChoosePath(currentLocation);
         }
+        exitFound = true;
+
+
     }
 
     void Update()
@@ -87,7 +110,7 @@ public class AIManager : MonoBehaviour
 
     private void MoveForward(int[] cell)
     {
-        //Debug.Log("moved to " + (cell[0] + 1) + "," + (cell[1] + 1));
+        Debug.Log("moved to " + (cell[0] + 1) + "," + (cell[1] + 1));
         visitedCells.Add(cell);
         path.Add(cell);
         currentLocation = cell;
@@ -95,7 +118,7 @@ public class AIManager : MonoBehaviour
 
     private void MoveBackward(int[] cell)
     {
-        //Debug.Log("moved to " + (cell[0] + 1) + "," + (cell[1] + 1));
+        Debug.Log("moved to " + (cell[0] + 1) + "," + (cell[1] + 1));
         path.Remove(path[path.Count - 1]);
         currentLocation = cell;
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,14 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public RandomMazeGenerator.Cell[,] cells;
+    [HideInInspector]
     public int[] initialLocation;
+    [HideInInspector]
     public int[] exitLocation;
+    [HideInInspector]
+    public int[] keyLocation1;
+    [HideInInspector]
+    public int[] keyLocation2;
 
     void Awake()
     {
@@ -27,22 +33,24 @@ public class GameManager : MonoBehaviour
 
     private int[] SetInitialLocation()
     {
-        //int width = cells.GetLength(0);
-        //int lenght = cells.GetLength(1);
+        /*
+        int width = cells.GetLength(0);
+        int lenght = cells.GetLength(1);
 
-        //int locationIndex = Random.Range(0, 4);
+        int locationIndex = Random.Range(0, 4);
 
-        //switch (locationIndex)
-        //{
-        //    case 0:
-        //        return new int[] { 0, 0 };
-        //    case 1:
-        //        return new int[] { 0, width - 1 };
-        //    case 2:
-        //        return new int[] { 0, lenght - 1 };
-        //    default:
-        //        return new int[] { width - 1, lenght - 1 };
-        //}
+        switch (locationIndex)
+        {
+            case 0:
+                return new int[] { 0, 0 };
+            case 1:
+                return new int[] { 0, width - 1 };
+            case 2:
+                return new int[] { 0, lenght - 1 };
+            default:
+                return new int[] { width - 1, lenght - 1 };
+        }
+        */
 
         return new int[] { 0, 0 };
     }
@@ -52,11 +60,25 @@ public class GameManager : MonoBehaviour
         return new int[] { cells.GetLength(0) - 1, cells.GetLength(1) - 1 };
     }
 
+    private int[] SetKeysLocations(int number)
+    {
+        if (number == 1)
+        {
+            return new int[] { 0, cells.GetLength(1) - 1 };
+        }
+        else
+        {
+            return new int[] { cells.GetLength(0) - 1, 0 };
+        }
+    }
+
     void Start()
     {
         cells = RandomMazeGenerator.instance.cells;
         initialLocation = SetInitialLocation();
         exitLocation = SetExitLocation();
+        keyLocation1 = SetKeysLocations(1);
+        keyLocation2 = SetKeysLocations(2);
     }
 
     void Update()
