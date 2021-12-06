9416f56b002011aca532e1acf2f58593b2f971ef
Soheil Salehinia <soheil.salehinia@gmail.com>
2021-12-06 00:43:47 +0000

    Percentage added to loading

--- Assets/Scripts/UI Scripts/Loading.cs
+++ Assets/Scripts/UI Scripts/Loading.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Loading : MonoBehaviour
 {
+    public Text percentage;
+
     private void Start()
     {
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        StartCoroutine(LoadAsync());
+    }
+
+    private IEnumerator LoadAsync()
+    {
+        AsyncOperation asyncOperation =  SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        while(asyncOperation.progress < 1)
+        {
+            percentage.text = (asyncOperation.progress * 100) + "%";
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
